49b7432b8a07e5a8e5235cfad8101e72fdd22d45
once-ler <htao@eztier.com>
2019-02-17 16:14:08 -0500

    Add HL7 to CaPatient task to integration module

--- integration/src/main/scala/Boot.scala
+++ integration/src/main/scala/Boot.scala
@@ -1,9 +1,11 @@
 package com.eztier.integration.hl7
 
 import com.eztier.integration.hl7.Hl7MongoToCassandra._
+import com.eztier.integration.hl7.CaPatientToCassandra._
 
 object Boot extends App {
-  val r = streamMongoToCassandra
+  // val r = streamMongoToCassandra
+  val r = streamCaPatientToCassandra
   // Should be 0 if there is nothing to process.
   println(r)
 }
---
+++ integration/src/main/scala/CaPatientToCassandra.scala
@@ -0,0 +1,67 @@
+package com.eztier.integration.hl7
+
+import java.text.SimpleDateFormat
+import java.time.{LocalDateTime, ZoneId, ZoneOffset}
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.datastax.driver.core.{Row, SimpleStatement}
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import com.eztier.adapter.Hl7CassandraAdapter
+import com.eztier.hl7mock.types._
+import com.eztier.datasource.cassandra.dwh.runners.{CommandRunner => CassandraCommandRuner}
+import com.eztier.datasource.cassandra.dwh.implicits.Transactors.xaCaPatient
+
+import scala.util.Success
+
+object CaPatientToCassandra {
+  implicit val actorSystem = ActorSystem(name = "integration-actor-system")
+  implicit val streamMaterializer = ActorMaterializer()
+  implicit val executionContext = actorSystem.dispatcher
+  implicit val logger = actorSystem.log
+
+  private def getLastCaPatientUploaded = {
+    val fut = CassandraCommandRuner.search[CaHl7, CaHl7Control](s"select create_date from dwh.ca_table_date_control where id = 'ca_hl_7_control' limit 1")
+
+    val fut2: Future[LocalDateTime] = fut
+      .map { rs =>
+        val row: Row = rs.one()
+        val dt = row.getTimestamp("create_date")
+
+        // dt.toInstant.atZone(ZoneId.of("America/New_York")).toLocalDateTime
+        dt.toInstant.atOffset(ZoneOffset.UTC).toLocalDateTime
+      }
+      .recover {
+        case _ => LocalDateTime
+          .parse("1970-01-01T00:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+      }
+
+    Await.result(fut2, 10 seconds)
+  }
+
+  def streamCaPatientToCassandra = {
+    val from = getLastCaPatientUploaded
+    val to = from.plusHours(3).minusSeconds(1)
+    // runWithRowFilter() will query from ca_table_date_control and look for id "ca_hl_7"
+    val r = xaCaPatient.flow.runWithRowFilter(s"create_date >= '${from.toString}' and create_date < '${to.toString}'", 10)
+    // Update date control
+    import com.eztier.cassandra.CaCommon.camelToUnderscores
+    import com.eztier.hl7mock.CaCommonImplicits._
+
+    val x = camelToUnderscores(CaHl7Control().getClass.getSimpleName)
+    val updateTime = Date.from(to.atOffset(ZoneOffset.UTC).toInstant)
+    val el = CaTableDateControl(Id = x, CreateDate = updateTime)
+    val insertStatement = el.getInsertStatement("dwh")
+    val qs = insertStatement.getQueryString()
+
+    val up = xaCaPatient.flow.provider.insertAsync(insertStatement)
+    val r2 = Await.result(up, 10 seconds)
+
+    r2
+
+  }
+}
