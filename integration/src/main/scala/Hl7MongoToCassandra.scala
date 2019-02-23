0b0b8ed1031659ba61c9ad1047bbbabc37b65a7a
once-ler <htao@eztier.com>
2019-02-22 19:04:38 -0500

    Refactor mongo to cassandra for performance

--- integration/src/main/scala/Hl7MongoToCassandra.scala
+++ integration/src/main/scala/Hl7MongoToCassandra.scala
@@ -2,10 +2,14 @@ package com.eztier.integration.hl7
 
 import java.time.format.DateTimeFormatter
 import java.time.{Instant, LocalDate, LocalDateTime, ZoneId}
+import java.util.Date
 
 import akka.actor.{ActorSystem, Scheduler}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
+import com.eztier.hl7mock.types.CaTableDateControl
+
+import scala.concurrent.Promise
 // For akka retry
 import akka.pattern.after
 
@@ -19,12 +23,25 @@ import com.eztier.datasource.mongodb.hl7.models.Hl7Message
 import com.eztier.hl7mock.types.{CaHl7, CaHl7Control, CaPatient, CaPatientControl}
 import com.eztier.stream.CommonTask.balancer
 
+import scala.collection.JavaConverters._
+import com.datastax.driver.core.{BatchStatement, ResultSet, ResultSetFuture}
+import com.datastax.driver.core.querybuilder.{Insert}
+import com.google.common.util.concurrent.{FutureCallback, Futures}
+import com.eztier.cassandra.CaCommon.camelToUnderscores
+import com.eztier.datasource.cassandra.dwh.implicits.Transactors._
+import com.eztier.hl7mock.Hapi
+import com.eztier.hl7mock.HapiToCaHl7Implicits._
+import com.eztier.hl7mock.CaHl7Implicits._
+import com.eztier.hl7mock.CaCommonImplicits._
+
 object Hl7MongoToCassandra {
   implicit val actorSystem = ActorSystem(name = "integration-actor-system")
   implicit val streamMaterializer = ActorMaterializer()
   implicit val executionContext = actorSystem.dispatcher
   implicit val scheduler = actorSystem.scheduler
   implicit val logger = actorSystem.log
+
+  val session = xaCaHl7.flow.provider.session
 
   def getLastHl7MessageUploaded = {
     val fut = CassandraCommandRuner.search[CaHl7, CaHl7Control](s"select create_date from dwh.ca_table_date_control where id = 'ca_hl_7_control' limit 1")
@@ -75,10 +92,85 @@ object Hl7MongoToCassandra {
     Future(m)
   }
 
+  val rawToCa = Flow[String].mapAsync(parallelism = 100) {
+    str =>
+      val m = Hapi.parseMessage(str)
+
+      val res: Option[CaHl7] = m match {
+        case Some(a) =>
+          val c: CaHl7 = a
+          Some(c)
+        case _ => None
+      }
+
+      Future(res)
+  }
+
+  val caToInsert = Flow[Option[CaHl7]].mapAsync(parallelism = 100) {
+    maybeCa =>
+      val ca = maybeCa.get
+      val caControl: CaHl7Control = ca
+
+      val ins1 = ca.getInsertStatement(keySpace)
+      val ins2 = caControl.getInsertStatement(keySpace)
+
+      // val qs = ins1.getQueryString()
+      Future(List[(Date, Insert)]((ca.CreateDate, ins1), (ca.CreateDate, ins2)))
+  }
+
+  // https://www.datastax.com/dev/blog/java-driver-async-queries
+  implicit def resultSetFutureToScala(f: ResultSetFuture): Future[ResultSet] = {
+    val p = Promise[ResultSet]()
+    Futures.addCallback(f,
+      new FutureCallback[ResultSet] {
+        def onSuccess(r: ResultSet) = p success r
+        def onFailure(t: Throwable) = p failure t
+      })
+    p.future
+  }
+
+  val batch = Flow[Seq[(Date, Insert)]].map {
+    tup =>
+      val stmts = tup.map(_._2)
+      val batchStatement = new BatchStatement(BatchStatement.Type.UNLOGGED).addAll(stmts.asJava)
+
+      val f: Future[ResultSet] = session.executeAsync(batchStatement)
+
+      f.recover{ case _ =>  None}
+
+      val f1 = retry(f, Seq(1.seconds, 5.seconds, 10.seconds, 30.seconds, 60.seconds))
+
+      Await.result(f1, Duration.Inf)
+
+      tup.map(_._1)
+  }
+
   def retry[T](f: => Future[T], delays: Seq[FiniteDuration])(implicit ec: ExecutionContext): Future[T] = {
     f recoverWith { case _ if delays.nonEmpty => after(delays.head, scheduler)(retry(f, delays.tail)) }
   }
 
+  val updateControl = Flow[Seq[Date]]
+    .map{
+      a =>
+        val uts = a.max
+        val c3 = CaTableDateControl(
+          Id = camelToUnderscores("CaHl7Control"),
+          CreateDate = uts
+        )
+        val ins3 = c3 getInsertStatement(keySpace)
+
+        val f: Future[ResultSet] = session.executeAsync(ins3)
+
+        f.recover{ case _ =>  None}
+
+        val f1 = retry(f, Seq(1.seconds, 5.seconds, 10.seconds, 30.seconds, 60.seconds))
+
+        Await.result(f1, Duration.Inf)
+
+        a.length
+    }
+
+  //
   def persistToCassandra = Flow[String].map { m =>
     val f = CassandraCommandRuner.update[CaHl7, CaHl7Control](m)
     f.recover{ case _ => 0 }
@@ -102,11 +194,25 @@ object Hl7MongoToCassandra {
         val r = s
           .via(messageToRaw)
           .filter(_ != null)
+          .via(rawToCa)
+          .filter(_ != None)
+          .via(caToInsert)
+          .mapConcat(identity)
+          .grouped(50)
+          .via(batch)
+          .via(updateControl)
+          .runWith(Sink.head)
+
+        /*
+        val r = s
+          .via(messageToRaw)
+          .filter(_ != null)
           .via(balancer(persistToCassandra,10))
           .log("Persist")
           .grouped(100000)
           .via(logProgress)
           .runWith(Sink.head)
+        */
 
         Await.result(r, Duration.Inf)
       case _ => 0
