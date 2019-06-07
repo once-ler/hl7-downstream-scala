f16476b8c5df6b5809b4ecbc4319bc500ca5ace3
once-ler <htao@eztier.com>
2019-06-07 13:07:42 -0400

    Include csv to cassandra function at start

--- integration/src/main/scala/Boot.scala
+++ integration/src/main/scala/Boot.scala
@@ -2,13 +2,17 @@ package com.eztier.integration.hl7
 
 import com.eztier.integration.hl7.Hl7MongoToCassandra._
 import com.eztier.integration.hl7.CaPatientToCassandra._
+import com.eztier.integration.hl7.CsvToCassandra._
 
 object Boot extends App {
   // val r = runMongoToCassandra
   // println(r)
 
-  val r2 = runCaPatientToCassandra
-  println(r2)
+  // val r2 = runCaPatientToCassandra
+  // println(r2)
+
+  val r3 = runCsvToCassandra()
+  println(r3)
 
   System.exit(0)
 
--- integration/src/main/scala/CsvToCassandra.scala
+++ integration/src/main/scala/CsvToCassandra.scala
@@ -1,12 +1,12 @@
 package com.eztier.integration.hl7
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import com.eztier.datasource.common.traits.WithCsvSuppport
-import com.eztier.integration.hl7.Hl7MongoToCassandra.{batch, caToInsert, rawToCa}
-import com.eztier.stream.CommonTask.balancer
+import akka.stream.{ActorMaterializer, FlowShape}
 
-import scala.concurrent.Future
+import com.eztier.datasource.common.traits.WithCsvSuppport
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import com.eztier.datasource.cassandra.dwh.implicits.Transactors._
 
 object CsvToCassandra extends WithCsvSuppport {
   implicit val actorSystem = ActorSystem(name = "integration-actor-system")
@@ -14,15 +14,18 @@ object CsvToCassandra extends WithCsvSuppport {
   implicit val executionContext = actorSystem.dispatcher
   implicit val logger = actorSystem.log
 
-  def runCsvToCassandra(filePathAndName: String) = {
-    importAsText(filePathAndName)
-      .via(rawToCa)
-      .filter(_ != None)
-      .filter(_.get.Id != null)
-      .via(caToInsert)
-      .via(balancer(batch,100))
+  def runCsvToCassandra(filePathAndName: String = "patient3.json") = {
+    val s = importAsText(filePathAndName)
 
+    val f = xaCaHl7.flow.runWithRawStringSource(s, 20)
+    val f2 = xaCaPatient.flow.runWithRawStringSource(s, 20)
 
+    val result = for {
+      r <- f
+      r2 <- f2
+    } yield (r, r2)
+
+    Await.result(result, Duration.Inf)
   }
 
 }
